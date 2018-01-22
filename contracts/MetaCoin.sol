527a8dc21ffb0eacdedc952537ac68dbc8e07590
Fläschner Gotthold <gotthold-flaeschner@hotmail.de>
2018-01-22 23:23:28 +0100

    Included a status of the proposal that later on can by changed via votes

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -1,23 +1,30 @@
 pragma solidity ^0.4.17;
 
+import "./ConvertLib.sol";
+
+
 contract MetaCoin {
 
 	uint public moon = 700;
-	bytes32[] public data;
+
+	enum Status{unset, consideration, back, burned}
 
 	struct Proposal {
 		string propname;
 		string Hash;
 		string data;
+		Status status;
 	}
 
 	Proposal public proposal;
 
 	function MetaCoin() {
 
-		proposal.propname="ConstructProp1";
-		proposal.data="ConstructProp2";
-		proposal.Hash="ConstructProp3";
+		
+		proposal.propname="ConstructProp";
+		proposal.data="ConstructProp";
+		proposal.Hash="ConstructProp";
+		proposal.status=Status.unset;
 	}
 
 	function sendCoin() public{
@@ -27,14 +34,16 @@ contract MetaCoin {
 	function getBalance() public view returns(uint) {
 		return moon;
 	}
-	
+
 	function sendData(string propname, string data, string Hash) public{
 		proposal.propname = propname;
 		proposal.Hash = Hash;
-		proposal.data = data;		
+		proposal.data = data;
+		proposal.status = Status.consideration;
+				
 	}
-	function getData() public view returns(string, string, string) {
-		return (proposal.propname, proposal.data, proposal.Hash);
+	function getData() public view returns(string, string, string, Status) {
+		return (proposal.propname, proposal.data, proposal.Hash, proposal.status);
 	}
 
 }
